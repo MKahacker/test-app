69caeb9a98d6e4971ba4a476a0fc5fa3790a2b5a
makk <mkahack@hawk.iit.edu>
2019-02-06 18:16:15 -0600

    Changed blob to text and short to string

--- db/migrate/20190207000341_create_moods.rb
+++ db/migrate/20190207000341_create_moods.rb
@@ -1,8 +1,8 @@
 class CreateMoods < ActiveRecord::Migration[5.2]
   def change
     create_table :moods do |t|
-      t.text :short
-      t.blob :note
+      t.string :short
+      t.text :note
 
       t.timestamps
     end
